24860373fe84754b1d0d2996dbf79bea7a5497ff
WinnieTrandinh <22330162+WinnieTrandinh@users.noreply.github.com>
2017-12-07 12:48:12 -0500

    Fix of previous commit.

---
+++ src/bots/TinFoil.java
@@ -0,0 +1,17 @@
+package bots;
+
+import roles.Role;
+
+public class TinFoil extends PrototypeLXI {
+
+	public TinFoil() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Role getRole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/bots/Ziploc.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package bots;
+
+import roles.Role;
+
+/**
+ * @author 1trandinhwin
+ *
+ */
+public class Ziploc extends PrototypeLXI {
+
+	/**
+	 * 
+	 */
+	public Ziploc() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public Role getRole() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
